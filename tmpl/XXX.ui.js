feccad20c531ab5e1a97ff7ab081f1a1b258afa2
Elliot Nunn <elliotnunn@me.com>
2019-05-02 13:42:26 +0800

    Progress bar! But I broke the graph.

--- tmpl/XXX.ui.js
+++ tmpl/XXX.ui.js
@@ -1,3 +1,43 @@
+kEnoughSims = 200;
+
+function seriousMainThingDoer()
+{
+  /* Terminate any workers that are running */
+  try {
+    for (var i = 0; i < gScenarioArray.length; i++) {
+      gScenarioArray[i].worker.terminate();
+    }
+    gScenarioArray = undefined;
+  } catch {
+    /* idempotent, do nothing */
+  }
+
+  /* Load user input (quite involved) */
+  var scenario_array = divideDomIntoScenarios(this); 
+
+  /* Do one-off input parsing for each calculated scenario */
+  for (var i = 0; i < scenario_array.length; i++) {
+    scenario_array[i].stdin = parseDomScenario(scenario_array[i].dom);
+  }
+
+  /* Callback to update the ui progress bar */
+  function update_callback()
+  {
+    updateUiProgress(scenario_array);
+  }
+
+  /* Call it once to zero the progress bar */
+  update_callback();
+
+  /* Start the workers */
+  for (var i = 0; i < scenario_array.length; i++) {
+    simScenario(scenario_array[i], update_callback);
+  }
+
+  /* Leave a global around to pick up later */
+  gScenarioArray = scenario_array;
+}
+
 function parse_time(in_str)
 {
   return parseFloat(in_str.substr(0, 2)) + parseFloat(in_str.substr(2, 4)) / 60;
@@ -9,7 +49,7 @@ function mkPath(x, y) {
   for (var i = 0; i < x.length; i++) {
     accum.push(cmd); cmd = "L";
     accum.push(x[i].toString());
-    accum.push((-y[i]).toString());
+    accum.push(100 - y[i].toString() * 100 / 50);
   }
   accum = accum.join(" ");
   newEl = document.createElementNS('http://www.w3.org/2000/svg',"path");
@@ -17,24 +57,62 @@ function mkPath(x, y) {
   return newEl;
 }
 
-function scrape()
+function divideDomIntoScenarios()
+{
+  user_input_element = document.querySelector(".OD-editable");
+
+  var N = 1;
+
+  // okay, now we just need to keep making dupes of the DOM element... easy!
+  var every_field = user_input_element.querySelectorAll("input");
+  for (var i = 0; i < every_field.length; i++) {
+    var text = every_field[i].value;
+    N *= text.split("/").length;
+  }
+
+  // each dupe of the DOM has a separate permutation of "slashed" fields
+  big_array = []
+  for (var i = 0; i < N; i++) {
+    var name = [];
+    var instance = user_input_element.cloneNode(true); /* deep copy of node */
+    var shrinkMod = i;
+    var shrinkN = N;
+    var every_field = instance.querySelectorAll("input");
+    for (var j = 0; j < every_field.length; j++) {
+      var possibilities = every_field[j].value.split("/");
+      var chunk = Math.floor(shrinkN / possibilities.length);
+      var choice = possibilities[Math.floor(shrinkMod / chunk)];
+
+      if (possibilities.length > 1) name.push(choice);
+      every_field[j].value = choice;
+
+      shrinkMod %= chunk;
+      shrinkN /= possibilities.length;
+    }
+    big_array.push({name: name.join("/"), dom: instance});
+  }
+
+  return big_array;
+}
+
+function parseDomScenario(dom)
 {
   /* Marshal a bayes input file from the history */
   var ary = [];
 
   /* Overall settings */
-  ary.push("MAX 50");
+  ary.push("MAX " + kEnoughSims.toString());
   ary.push("TRY 10000000");
 
   /* Marshal covariates */
-  #define X(short, long, unit) ary.push("PARAM " + long + " " + document.getElementById(<DOSTRING>short</DOSTRING>).value + " " + unit);
+  #define X(short, long, unit) ary.push("PARAM " + long + " " + dom.querySelector("#" + <DOSTRING>short</DOSTRING>).value + " " + unit);
   MOD_X_PARAMS
   #undef X
 
   var maxTime = 0; /* How far out to take our graph */
 
   /* Marshal doses (this format will change) */
-  var lines = document.querySelectorAll(".OD-inputline-dose");
+  var lines = dom.querySelectorAll(".OD-inputline-dose");
   for (var i = 0; i < lines.length; i++) {
     function X(cls) {return lines[i].getElementsByClassName(cls)[0].value;}
 
@@ -48,7 +126,7 @@ function scrape()
   }
 
   /* Marshal levels */
-  var lines = document.querySelectorAll(".OD-inputline-level");
+  var lines = dom.querySelectorAll(".OD-inputline-level");
   for (var i = 0; i < lines.length; i++) {
     function X(cls) {return lines[i].getElementsByClassName(cls)[0].value;}
 
@@ -63,63 +141,46 @@ function scrape()
 
   /* How many time points would we like for our graph? */
   var numPoints = 501;
-  x = []
   for (var i = 0; i < numPoints; i++) {
     var hour = xWidth / (numPoints - 1) * i;
-    x.push(hour);
     ary.push(hour.toString() + " h GET");
   }
 
   ary.push(""); // trailing newline needed
   query = ary.join("\n");
-
-  etas = []; for (var i = 0; i < 4; i++) etas.push([]);
-  points = []; for (var i = 0; i < numPoints; i++) points.push([]);
-  isFirstChunk = true;
-
-  document.getElementById("plot").innerHTML = "";
-
-  if (typeof hardWorker != 'undefined') hardWorker.terminate();
-  hardWorker = new Worker("XXX.worker.js");
-
-  hardWorker.onmessage = function (msg) {
-    outputChunk = msg.data;
-    if (isFirstChunk) {
-      isFirstChunk = false;
-      return;
-    }
-
-    newEta = outputChunk[0].split(' ').slice(1).map(Number);
-    for (var i = 0; i < newEta.length; i++) {
-      etas[i].push(newEta[i]);
-    }
-
-    newPoints = []
-    for (var i = 0; i < outputChunk.length - 1; i++) {
-      thisLine = outputChunk[i + 1];
-      thisVal = thisLine.split(' ')[3];
-      newPoints.push(thisVal);
-    }
-    for (var i = 0; i < newPoints.length; i++) {
-      points[i].push(newPoints[i]);
-    }
-
-    thePath = mkPath(x, newPoints);
-    thePath.setAttributeNS(null, "fill", "transparent");
-    thePath.setAttributeNS(null, "stroke", "blue");
-    thePath.setAttributeNS(null, "stroke-width", "0.05px");
-    document.getElementById("plot").appendChild(thePath);
-  }
-
-  hardWorker.onclose = function () {
-    console.log('Completed baypk run in Web Worker');
-  }
-
-  hardWorker.postMessage(query);
-
-  /* next task: get ajaxing!! */
+  return query;
 }
 
+function simScenario(scenario, callback)
+{
+  // scenario has members: name, dom, stdin, now worker and soon stdout
+  var hardWorker = new Worker("XXX.worker.js");
+  scenario.worker = hardWorker;
+  scenario.stdout = [];
+
+  hardWorker.onmessage = function (msg) {
+    scenario.stdout.push(msg.data);
+    callback();
+  }
+
+  hardWorker.postMessage(scenario.stdin);
+}
+
+function updateUiProgress(scenario_array)
+{
+
+  /* urgent: this needs a rewrite! */
+  var sims_done = 0;
+  for (var i = 0; i < scenario_array.length; i++) {
+    if (typeof scenario_array[i].stdout != "undefined") {
+      sims_done += scenario_array[i].stdout.length - 1;
+    }
+  }
+
+  var prog = document.querySelector("progress");
+  prog.max = kEnoughSims * scenario_array.length;
+  prog.value = sims_done;
+}
 
 // Code to edit the dosing history
 function OD_new_dose (caller) {
