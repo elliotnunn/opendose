29e870ac6831a57f05d17b740cf820513574ba6c
Elliot Nunn <elliotnunn@me.com>
2019-05-19 12:27:09 +0800

    Use HTML5 templates for history entry
    This will make it easier to convert patient histories to a terse textual
    format.

--- tmpl/XXX.ui.js
+++ tmpl/XXX.ui.js
@@ -1,62 +1,10 @@
 // Code to edit the dosing history
 function OD_new_dose (caller) {
-  html = 
-    '<div class="list-group-item OD-inputline-dose"><form class="form-inline">' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<button class="btn btn-danger btn-sm" onclick="OD_enter_dirtymode(); this.parentNode.parentNode.parentNode.remove();">Del</button>' +
-      '</div>' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<div class="input-group-prepend">' +
-          '<span class="input-group-text">day</span>' +
-        '</div>' + 
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-date" size="4" value="1"></input>' +
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-time" size="4" value="0000"></input>' +
-      '</div>' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-dose" size="4" value="1000"></input>' +
-        '<div class="input-group-append">' +
-          '<span class="input-group-text">' + MOD_DRUG_UNIT + '</span>' +
-        '</div>' + 
-      '</div>' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<div class="input-group-prepend">' +
-          '<span class="input-group-text">given over</span>' +
-        '</div>' + 
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-dosepush" size="4" value="1"></input>' +
-        '<div class="input-group-append">' +
-          '<span class="input-group-text">' + MOD_TIME_UNIT + '</span>' +
-        '</div>' + 
-      '</div>' +
-    '</form></div>';
-
-  document.querySelector(".OD-insertbeforeme").insertAdjacentHTML("beforebegin", html);
+  document.querySelector(".OD-appendtome").insertBefore(document.querySelector("#dose").content.cloneNode(true), document.querySelector(".OD-insertbeforeme"));
 }
 
 function OD_new_level (caller) {
-  html = 
-    '<div class="list-group-item OD-inputline-level"><form class="form-inline">' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<button class="btn btn-danger btn-sm" onclick="OD_enter_dirtymode(); this.parentNode.parentNode.parentNode.remove();">Del</button>' +
-      '</div>' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<div class="input-group-prepend">' +
-          '<span class="input-group-text">day</span>' +
-        '</div>' + 
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-date" size="4" value="1"></input>' +
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-time" size="4" value="0000"></input>' +
-      '</div>' +
-      '<div class="input-group input-group-sm mx-1">' +
-        '<div class="input-group-prepend">' +
-          '<span class="input-group-text">TDM level</span>' +
-        '</div>' + 
-        '<input type="text" onclick="OD_enter_dirtymode();" class="form-control OD-input-level" size="3" value="0"></input>' +
-        '<div class="input-group-append">' +
-          '<span class="input-group-text">mg/L</span>' +
-        '</div>' + 
-      '</div>' +
-    '</form></div>';
-
-  document.querySelector(".OD-insertbeforeme").insertAdjacentHTML("beforebegin", html);
+  document.querySelector(".OD-appendtome").insertBefore(document.querySelector("#level").content.cloneNode(true), document.querySelector(".OD-insertbeforeme"));
 }
 
 function OD_enter_dirtymode(caller, validator) {
