e6afa54f0c86ff0f78a6de65086132d3a1d4c8bd
Elliot Nunn <elliotnunn@me.com>
2019-06-08 22:03:19 +0800

    Rudiments of new UI

--- tmpl/XXX.ui.js
+++ tmpl/XXX.ui.js
@@ -1,10 +1,6 @@
 // Code to edit the dosing history
-function OD_new_dose (caller) {
-  document.querySelector(".OD-appendtome").insertBefore(document.querySelector("#dose").content.cloneNode(true), document.querySelector(".OD-insertbeforeme"));
-}
-
-function OD_new_level (caller) {
-  document.querySelector(".OD-appendtome").insertBefore(document.querySelector("#level").content.cloneNode(true), document.querySelector(".OD-insertbeforeme"));
+function cloneTmpl (sel) {
+  return document.importNode(document.querySelector(sel).content, true);
 }
 
 function OD_enter_dirtymode(caller, validator) {
@@ -20,4 +16,25 @@ function OD_exit_dirtymode(caller, validator) {
       durationel.value = x.value;
     }
   })
+}
+
+function OD_key(event) {
+  if (event.code == 'Backspace' &&
+    event.type == 'keyup' && /* prevents key-repeat from clobbering the hx */
+    event.target.selectionStart == 0 && /* already backspaced to start of field */
+    document.querySelectorAll('section.history tr').length > 2) /* not deleting the last one */
+  {
+    var del_targ = event.target.parentNode.parentNode;
+    var to_focus = del_targ.nextElementSibling || del_targ.previousElementSibling;
+    del_targ.remove();
+    to_focus.querySelector('input').focus();
+  }
+
+  if (event.code == 'Enter' && event.type == 'keyup') {
+    var dup_targ = event.target.parentNode.parentNode;
+    var new_el = dup_targ.cloneNode(true);
+    new_el.querySelectorAll('input')[2] = ''; /* levels should never be duplicated */
+    dup_targ.after(new_el);
+    new_el.querySelectorAll('input')[0].focus();
+  }
 }
